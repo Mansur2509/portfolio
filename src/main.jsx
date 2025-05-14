7bb359bd812432c6da977368fdb1d0100b1bb060
Azamat <azamatnizomutdinov@gmail.com>
2025-05-14 14:10:41 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,10 @@ createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </StrictMode>,
 )
+// echo "# f" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Mansur2509/f.git
+// git push -u origin main
